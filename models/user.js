e506a80024501f866a6f2c12028982e5a7252a35
sagar2526 <sagarhinge01@gmail.com>
2018-09-09 18:16:26 +0530

    user model added

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+var UserSchema = new mongoose.Schema({
+    username:	String,
+    email: String,
+    password: String,
+    confirmPassword: String,
+    firstName: String,
+    middleName: String,
+    lastName: String,
+    dob: Date,
+    gender: String,
+    token: Array,
+    createdAt: Date,
+    updatedAt: Date,
+})
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
